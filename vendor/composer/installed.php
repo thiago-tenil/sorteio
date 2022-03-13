b3fd65eeac810d39e8169c1627e9f58782b9b689
Thiago Albuquerque de Paula <62264928+thiago-tenil@users.noreply.github.com>
2022-03-13 16:55:58 -0300

    Sistema de sorteio
    commit do sistema de sorteio

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd64e031c8a9c873ac58f45c858f8204c6a6417e8',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd64e031c8a9c873ac58f45c858f8204c6a6417e8',
+            'dev_requirement' => false,
+        ),
+        'slim/slim' => array(
+            'pretty_version' => '2.6.3',
+            'version' => '2.6.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../slim/slim',
+            'aliases' => array(),
+            'reference' => '9224ed81ac1c412881e8d762755e3d76ebf580c0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
